fbd01ab259edf51ad4c2e357fbf373c4a3290c2b
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-02 00:34:57 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "sns_topic_name" {
+  description = "Name of the SNS topic"
+  type        = string
+  default     = "my-topic"
+}
+
+variable "email_subscription" {
+  description = "Email address to subscribe to the SNS topic"
+  type        = string
+}
